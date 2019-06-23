ced71714861745062d1c5d2521d2998062399364
CJHydraGenZ <cahyasaga321@gmail.com>
2019-06-23 23:09:40 +0800

    css desain

--- index.php
+++ index.php
@@ -179,6 +179,7 @@ foreach ($result as $getGambar) {;
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="assets/js/patikel/particles.js-master/particles.js"></script>
     <script src="assets/js/patikel/particles.js-master/demo/js/app.js"></script>
+    <script src="assets/js/main.js  "></script>
     <script src="assets/js/mainIn.js"></script>
 
 </body>
